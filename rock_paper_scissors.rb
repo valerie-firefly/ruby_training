1353b5af413746adbf0185a1ef8f8f0cf6d432ed
Valerie Sifton <69983176+valerie-firefly@users.noreply.github.com>
2021-07-07 16:05:15 -0400

    Add files via upload

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,37 @@
+    def game_result(p1,p2)
+        if (p1.upcase == "ROCK" && p2.upcase == "SCISSORS") || (p2.upcase == "ROCK" && p1.upcase == "SCISSORS")
+            return "Rock crushes scissors."
+        elsif (p1.upcase == "ROCK" && p2.upcase == "PAPER") || (p2.upcase == "ROCK" && p1.upcase == "PAPER")
+            return "Paper covers rock."
+        elsif (p1.upcase == "SCISSORS" && p2.upcase == "PAPER") || (p2.upcase == "SCISSORS" && p1.upcase == "PAPER")
+            return "Scissors cut paper."
+        else
+            return "It's a tie"
+        end
+   end
+
+    def get_valid_answer
+        answer = ""
+        valid_answer = false
+        until valid_answer 
+            answer = gets.chomp
+            valid_answer = (answer.upcase == "ROCK" || answer.upcase == "PAPER" || answer.upcase == "SCISSORS")
+            puts "No cheating!  Rock, paper, scissors only!!!" if !valid_answer
+        end
+        return answer
+    end    
+
+    puts "Let's play rock paper scissors!"
+    player1 = ""
+    player2 = ""
+    p1_valid = false
+    p2_valid = false
+
+    puts "Player 1:  "
+    player1 = get_valid_answer
+
+    puts "Player 2:  "
+    player2 = get_valid_answer
+
+    puts game_result(player1, player2)
+
