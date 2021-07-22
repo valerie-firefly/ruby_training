6b8927b5be932162d5ddeb869cca31da2cff5c34
Valerie Sifton <69983176+valerie-firefly@users.noreply.github.com>
2021-07-22 11:58:17 -0400

    Update chapter6-reviews.rb
    Introducing a faster method..

--- chapter6-reviews.rb
+++ chapter6-reviews.rb
@@ -6,12 +6,6 @@ lines=[]
 file.open("reviews.txt") do |review_file|
     lines = review_file.readlines
 end
-
-relevant_lines = []
-lines.each do |line|
-    if line.include?("Truncated")
-        relevant_lines << line
-    end
-end
+relevant_lines = lines.find_all { |line| line.include?("Truncated")}
 
 puts relevant_lines
